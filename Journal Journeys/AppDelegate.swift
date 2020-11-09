cbb127923a5551a5ad4b57376cf0c1196062c34d
Kimberly Le <lekimberly022@gmail.com>
2020-11-09 17:51:18 -0500

    the login screen and code

---
+++ Journal Journeys/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  Journal Journeys
+//
+//  Created by Kimberly Le on 11/9/20.
+//
+
+import UIKit
+import Parse
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "cDgVeNSn0c5stNhD4zo8uknq6MoP47X5WCmC3FXW"
+                $0.clientKey = "0vW399LCMV3CEsYrAPWaKVBhxUbfntMDtZ9ztKdt"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        if PFUser.current() !=  nil{ //and if they are don't take them to the login page but take them to the navigation controller which has the view controller
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let homeFeedNavigationController = main.instantiateViewController(withIdentifier: "HomeFeedNavigationController") //this instantiates HomeFeedNavigationController to have an instance of this nav controller
+            
+            window?.rootViewController = homeFeedNavigationController //change the rootViewController to immediately show the homeFeedNavController
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Journal Journeys' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Journal Journeys
+  pod "Parse"
+  pod "AlamofireImage"
+
+  target 'Journal JourneysTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Journal JourneysUITests' do
+    # Pods for testing
+  end
+
+end
