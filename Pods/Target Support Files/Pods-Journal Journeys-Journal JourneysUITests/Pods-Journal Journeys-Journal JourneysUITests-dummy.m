5bee33541357d7ad2ace961af7ace85e81af441c
thuyvanv <50762956+thuyvanv@users.noreply.github.com>
2020-11-09 19:08:33 -0500

    dummy

---
+++ Pods/Target Support Files/Pods-Journal Journeys-Journal JourneysUITests/Pods-Journal Journeys-Journal JourneysUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Journal_Journeys_Journal_JourneysUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Journal_Journeys_Journal_JourneysUITests
+@end
